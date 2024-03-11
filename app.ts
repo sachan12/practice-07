76ec6af88965824a33d274cbe23fde6724ffd1a3
sachan12 <sachinnanda427@gmail.com>
2024-03-11 12:25:16 -0700

    add files

---
+++ app.ts
@@ -0,0 +1,89 @@
+// Arithmetic and comparing Operators ;
+// Arithematic Operators (yee hum (number data type) pai lagate hain);
+// 1-Addition
+
+// console.log (10+10) ;
+// console.log(20+20);
+// console.log(30+30);
+
+// let num1 = 20;
+// let num2 = 10;
+// let num3 = '5';
+// let num4 = 20;
+// let sum = num1 + num2;
+// console.log (sum);
+
+// // 2- Substraction;
+// let difference = num1 - num2 ;
+// console.log(difference);
+
+
+
+// // 3-Multiplication;
+// let product = num1 * num2 ;
+// console.log (product);
+
+// // 4-Division
+// let quotient = num1 / num2 ;
+// console.log(quotient);
+
+// // Common Example;
+// console.log ((200-100)*10/2)
+
+// // Modulo (%) it provides remainder of two numbers;
+// let remainder = num1%num2;
+// console.log(remainder);
+// console.log(10%3);
+
+
+// Relational / Comparison Operators  (  == , === , !==  , > , < , <= , >= , )  ==> Answer id Boolean (true/false)
+// let num1 = 20;
+// let num2 = 10;
+// let num3 = '5';
+// let num4 = 20;
+// let num5 = 5 ;
+// Equals
+// console.log (num1 == num2);
+// console.log (num1 === num2);
+// console.log (num3 === num5);
+
+// // Not Equal to (!==)
+// let age = 25
+// console.log (age!==25);
+// console.log (age !== 20);
+
+// // Less than
+// console.log(num2<num1);
+// console.log(num1<num2);
+
+// // Less than or equal to (<=);
+// console.log(num2<=num1);
+// console.log(num1<=num2);
+
+// // Greater than or equal to (>=);
+// console.log(num1 >= num2);
+// console.log(num2 >= num1);
+
+
+// Question -23 
+// let car = 'subaru';
+// let bike = 'subaru';
+// console.log( car == 'subaru');
+// console.log(car >= "subaru");
+// console.log(car <= "subaru");
+// console.log(car == bike);
+// console.log(bike == car);
+
+// console.log(car !== 'subaru');
+// console.log(car > 'subaru');
+// console.log(car < 'subaru');
+// console.log(bike > car);
+// console.log(car > bike);
+
+
+
+
+
+
+
+
